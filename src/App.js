24da51998238f57be9ae3a3f1da94bd138838a32
visaljudan <visal.nls2003@gmail.com>
2024-06-18 18:09:15 +0700

    Create CRUD category page

--- src/App.js
+++ src/App.js
@@ -1,44 +1,51 @@
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-import Homepage from './Pages/Homepage';
-import BrowsePage from './Pages/BrowsePage';
-import SignUpPage from './Pages/SignUpPage';
-import SignInPage from './Pages/SignInPage';
-import ItemDetailPage from './Pages/ItemDetailPage';
-import CreatePostPage from './Pages/CreatePostPage';
-import AboutUsPage from './Pages/AboutUsPage';
+import Homepage from "./Pages/Homepage";
+import BrowsePage from "./Pages/BrowsePage";
+import SignUpPage from "./Pages/SignUpPage";
+import SignInPage from "./Pages/SignInPage";
+import ItemDetailPage from "./Pages/ItemDetailPage";
+import CreatePostPage from "./Pages/CreatePostPage";
+import AboutUsPage from "./Pages/AboutUsPage";
 
-import AdminDashboardPage from './Pages/Admin/DashboardPage';
-import AdminUsersPage from './Pages/Admin/UsersPage';
-import AdminUserEditPage from './Pages/Admin/Users/EditPage';
-import AdminPropertyPage from './Pages/Admin/PropertyPage';
+import AdminDashboardPage from "./Pages/Admin/DashboardPage";
+import AdminUsersPage from "./Pages/Admin/UsersPage";
+import AdminUserEditPage from "./Pages/Admin/Users/EditPage";
+import AdminPropertyPage from "./Pages/Admin/PropertyPage";
+import AdminCategoryPage from "./Pages/Admin/CategoryPage";
 
-import SellerPropertyPage from './Pages/Seller/PropertyPage';
+import SellerPropertyPage from "./Pages/Seller/PropertyPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path='/'>
+    <Route path="/">
       <Route index element={<Homepage />} />
-      <Route path='browse' element={<BrowsePage />} />
-      <Route path='sign-up' element={<SignUpPage />} />
-      <Route path='sign-in' element={<SignInPage />} />
-      <Route path='create-post' element={<CreatePostPage />} />
-      <Route path='item/:id' element={<ItemDetailPage />} />
-      <Route path='about-us' element={<AboutUsPage />} />
+      <Route path="browse" element={<BrowsePage />} />
+      <Route path="sign-up" element={<SignUpPage />} />
+      <Route path="sign-in" element={<SignInPage />} />
+      <Route path="create-post" element={<CreatePostPage />} />
+      <Route path="item/:id" element={<ItemDetailPage />} />
+      <Route path="about-us" element={<AboutUsPage />} />
 
-      <Route path='admin'>
+      <Route path="admin">
         <Route index element={<AdminDashboardPage />} />
-        <Route path='dashboard' element={<AdminDashboardPage />} />
-        <Route path='users' element={<AdminUsersPage />} />
-        <Route path='users/:id/edit' element={<AdminUserEditPage />} />
-        <Route path='properties' element={<AdminPropertyPage />} />
+        <Route path="dashboard" element={<AdminDashboardPage />} />
+        <Route path="users" element={<AdminUsersPage />} />
+        <Route path="users/:id/edit" element={<AdminUserEditPage />} />
+        <Route path="properties" element={<AdminPropertyPage />} />
+        <Route path="categories" element={<AdminCategoryPage />} />
       </Route>
 
-      <Route path='seller'>
+      <Route path="seller">
         <Route index element={<AdminDashboardPage />} />
-        <Route path='dashboard' element={<AdminDashboardPage />} />
-        <Route path='properties' element={<SellerPropertyPage />} />
-        <Route path='create-post' element={<CreatePostPage />} />
+        <Route path="dashboard" element={<AdminDashboardPage />} />
+        <Route path="properties" element={<SellerPropertyPage />} />
+        <Route path="create-post" element={<CreatePostPage />} />
       </Route>
     </Route>
   )
--- src/Layouts/AdminLayout.jsx
+++ src/Layouts/AdminLayout.jsx
@@ -1,64 +1,62 @@
-import { useEffect, useState } from 'react';
-import Nav from '../components/Nav';
-import Sidebar from '../components/Sidebar';
-import { useDispatch } from 'react-redux';
-import { logIn, logOut } from '../redux/slices/authSlice';
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useEffect, useState } from "react";
+import Nav from "../components/Nav";
+import Sidebar from "../components/Sidebar";
+import { useDispatch } from "react-redux";
+import { logIn, logOut } from "../redux/slices/authSlice";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function AdminLayout({ children }) {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const user = useSelector((state) => state.auth.value);
+  // const dispatch = useDispatch();
+  // const navigate = useNavigate();
+  // const user = useSelector((state) => state.auth.value);
 
-  const [loading, SetLoading] = useState(false);
+  // const [loading, SetLoading] = useState(false);
 
-  const getMe = async () => {
-    const url = process.env.REACT_APP_API_URL + '/me';
-    const accessToken = localStorage.getItem("access_token");
+  // const getMe = async () => {
+  //   const url = process.env.REACT_APP_API_URL + '/me';
+  //   const accessToken = localStorage.getItem("access_token");
 
-    await fetch(url, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${accessToken}`
-      }
-    })
-      .then(response => response.json())
-      .then(async data => {
-        // Do something with the response data
-        dispatch(logIn({ email: data.email, id: data.id, role_id: data.role_id }));
-        checkRole(data.role_id);
-        SetLoading(true);
-      })
-      .catch(error => {
-        // Handle any errors
-        navigate('/sign-in');
-      });
-  }
+  //   await fetch(url, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Authorization': `Bearer ${accessToken}`
+  //     }
+  //   })
+  //     .then(response => response.json())
+  //     .then(async data => {
+  //       // Do something with the response data
+  //       dispatch(logIn({ email: data.email, id: data.id, role_id: data.role_id }));
+  //       checkRole(data.role_id);
+  //       SetLoading(true);
+  //     })
+  //     .catch(error => {
+  //       // Handle any errors
+  //       navigate('/sign-in');
+  //     });
+  // }
 
-  const checkRole = (role_id) => {
-    if (role_id == 2) {
-      navigate("/")
-    }
-  }
+  // const checkRole = (role_id) => {
+  //   if (role_id == 2) {
+  //     navigate("/")
+  //   }
+  // }
 
-  useEffect(() => {
-    getMe();
-  }, []);
+  // useEffect(() => {
+  //   getMe();
+  // }, []);
 
-  if (!loading && !user.id) {
-    return <div>Loading...</div>;
-  }
+  // if (!loading && !user.id) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <div>
       <Nav />
       <Sidebar />
       <div className="p-4 sm:ml-64 mt-[68px]">
-        <div className="p-4">
-          {children}
-        </div>
+        <div className="p-4">{children}</div>
       </div>
     </div>
   );
